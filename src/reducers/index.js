d8596353633d11ed79978423a6c80e8882e219ff
Nick Zhang <nick976183@gmail.com>
2020-03-08 06:55:34 -0700

    Added entity reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,38 @@
 import { combineReducers } from "redux";
 import articlesReducer from "./articles";
 import commonReducer from "./common";
+import merge from "lodash/merge";
+import feed from "./feed";
+import {
+  ARTICLES_REQUEST,
+  ARTICLES_SUCESS,
+  ARTICLES_FAILURE
+} from "../constants/actionTypes";
+
+const defaultEntityState = {
+  users: {},
+  articles: {}
+};
+
+const entitiesReducer = (state = defaultEntityState, action) => {
+  if (action.response && action.response.entities) {
+    return merge({}, state, action.response.entities);
+  }
+  return state;
+};
+
+const personalFeed = feed({
+  types: [ARTICLES_REQUEST, ARTICLES_SUCESS, ARTICLES_FAILURE]
+});
+
+const feeds = combineReducers({
+  personalFeed
+});
 
 const rootReducer = combineReducers({
-  commonState: commonReducer,
-  articlesState: articlesReducer
+  common: commonReducer,
+  entities: entitiesReducer,
+  feeds: feeds
 });
 
 export default rootReducer;
